3f3d97e7d18cf505fe7dc5a652a728d8cfefd701
delve <delve202@gmail.com>
2014-11-17 20:02:23 -0500

    submitting leap solution

---
+++ csharp/leap/Leap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.leap
+{
+    class Year
+    {
+        public static bool IsLeap(int year)
+        {
+            if (year % 400 == 0)
+                return true;
+            if (year % 100 == 0)
+                return false;
+            if (year % 4 == 0)
+                return true;
+            // All special conditions fall through to false
+            return false;
+        }
+    }
+}
--- csharp/leap/LeapTest.cs
+++ csharp/leap/LeapTest.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Exercism.leap;
 
 [TestFixture]
 public class LeapTest
@@ -9,21 +10,18 @@ public class LeapTest
         Assert.That(Year.IsLeap(1996), Is.True);
     }
 
-    [Ignore]
     [Test]
     public void Invalid_leap_year()
     {
         Assert.That(Year.IsLeap(1997), Is.False);
     }
 
-    [Ignore]
     [Test]
     public void Turn_of_the_20th_century_is_not_a_leap_year()
     {
         Assert.That(Year.IsLeap(1900), Is.False);
     }
 
-    [Ignore]
     [Test]
     public void Turn_of_the_25th_century_is_a_leap_year()
     {
