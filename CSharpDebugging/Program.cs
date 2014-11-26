b309a59faabc2af5b793d9c5d0409d23e30c05e9
delve <delve202@gmail.com>
2014-11-26 15:14:51 -0500

    submitting anagram exercism

---
+++ CSharpDebugging/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDebugging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var detector = new Exercism.anagram.Anagram("Orchestra");
+            var words = new[] { "cashregister", "Carthorse", "radishes" };
+            var results = new[] { "Carthorse" };
+            detector.Match(words);
+        }
+    }
+}
---
+++ csharp/anagram/anagram.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.anagram
+{
+    public class Anagram
+    {
+        public string MasterWord { get; set; }
+        public Dictionary<char, int> MasterHash { get; set; }
+
+        public Anagram(string master)
+        {
+            this.MasterWord = master.ToLower();
+            this.MasterHash = HashString(this.MasterWord);
+        }
+
+        public string[] Match(string[] candidates)
+        {
+            List<string> results = new List<string>();
+
+            foreach (string mixedCaseCandidate in candidates)
+            {
+                string candidate = mixedCaseCandidate.ToLower();
+
+                if (candidate.Length != this.MasterWord.Length) continue;
+                if (candidate == MasterWord) continue;
+                if (DictionaryEquals(this.MasterHash, HashString(candidate)))
+                    results.Add(mixedCaseCandidate);
+            }
+
+            return results.ToArray();
+        }
+
+        private Dictionary<char, int> HashString(string input)
+        {
+            Dictionary<char, int> output = new Dictionary<char, int>();
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (output.ContainsKey(input[i]))
+                    output[input[i]] = ++output[input[i]];
+                else
+                    output.Add(input[i], 1);
+            }
+
+            return output;
+        }
+
+        public static bool DictionaryEquals<TKey, TVal>(IDictionary<TKey,TVal> left, IDictionary<TKey,TVal> right)
+        {
+            if (left == right) return true;
+            if (left.Count != right.Count) return false;
+            if (left == null || right == null) return false;
+
+            IEqualityComparer<TVal> compare = EqualityComparer<TVal>.Default;
+            foreach (KeyValuePair<TKey,TVal> leftPair in left)
+            {
+                TVal rightVal;
+                if (!right.TryGetValue(leftPair.Key, out rightVal)) return false;
+                if (!compare.Equals(leftPair.Value, rightVal)) return false;
+            }
+
+            return true;
+        }
+    }
+}
