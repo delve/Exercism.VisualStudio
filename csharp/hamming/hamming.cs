9fd452a36d03446a2d4760b0027023cbc070f366
delve <delve202@gmail.com>
2014-11-27 15:02:49 -0500

    submitting hamming

---
+++ csharp/hamming/hamming.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercism.hamming
+{
+    public static class Hamming
+    {
+        public static int Compute(string left, string right)
+        {
+            left = left.ToUpper();
+            right = right.ToUpper();
+            if (left.Length > right.Length)
+                left = left.Substring(0, right.Length);
+            else if (right.Length > left.Length)
+                right = right.Substring(0, left.Length);
+            if (left.Length == 0)
+                return 0;
+
+            int hammingDiff = 0;
+            for (int i = 0; i < left.Length; i++)
+            {
+                if (left[i] != right[i])
+                    ++hammingDiff;
+            }
+
+            return hammingDiff;
+        }
+    }
+}
