09bb0cca165a5b993ecfb3c69ddfede828ed7376
delve <delve202@gmail.com>
2014-11-28 07:25:58 -0500

    initial work

---
+++ csharp/word-count/word-count.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercism.word_count
+{
+    public class Phrase
+    {
+        public string InputPhrase { get; set; }
+
+        public Phrase(string input)
+        {
+            this.InputPhrase = input.ToLower();
+        }
+
+        public Dictionary<string, int> WordCount()
+        {
+
+            return new Dictionary<string,int>();
+        }
+    }
+}
