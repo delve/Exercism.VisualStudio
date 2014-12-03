5a5286dc146a996aa9f11425871d7c4c7a5dfdf7
delve <delve202@gmail.com>
2014-12-03 10:17:31 -0500

    cleaned up length checking, simplified .Zip() expression

--- csharp/hamming/hamming.cs
+++ csharp/hamming/hamming.cs
@@ -7,14 +7,17 @@ namespace Exercism.hamming
         {
             left = left.ToUpper();
             right = right.ToUpper();
-            if (left.Length > right.Length)
-                left = left.Substring(0, right.Length);
-            else if (right.Length > left.Length)
-                right = right.Substring(0, left.Length);
-            if (left.Length == 0)
-                return 0;
+            if (left.Length != right.Length)
+            {
+                int minLen = System.Math.Min(left.Length, right.Length);
+                if (minLen == 0)
+                    return 0;
+                left = left.Substring(0, minLen);
+                right = right.Substring(0, minLen);
+            }
 
-            return (left.Zip(right, (lchar, rchar) => lchar != rchar ? 1 : 0)).Sum();
+            return left.Zip(right, (lchar, rchar) => lchar != rchar)
+                .Count(val => val);
         }
     }
 }
